620d548f1145543a38984c9634d45eda2dfa53dc
antoinechedin <antoine.chedin@gmail.com>
2019-10-28 00:16:32 +0100

    Add random sphere scene

--- main.cpp
+++ main.cpp
@@ -38,6 +38,29 @@ std::string timeFormat(const int &milliseconds) {
     return ss.str();
 }
 
+std::vector<Hittable *> randomSpheres(const int &numSpheres, std::default_random_engine &g) {
+    std::vector<Hittable *> spheres;
+    std::uniform_real_distribution p(-100.0f, 100.0f);
+    std::uniform_real_distribution r(1.0f, 3.0f);
+    std::uniform_real_distribution t(0.0f, 1.0f);
+
+    spheres.push_back(new Sphere(Vec3(0, -10000, 0), 10000, new Lambertian(Vec3(0.5f, 0.5f, 0.5f))));
+    for (int i = 0; i < numSpheres; ++i) {
+        float type = t(g);
+        if (type < .8f) {
+            float radius = r(g);
+            spheres.push_back(new Sphere(Vec3(p(g), radius, p(g)), radius, new Lambertian(Vec3(t(g), t(g), t(g)))));
+        } else if (type < 0.9f) {
+            float radius = r(g);
+            spheres.push_back(new Sphere(Vec3(p(g), radius, p(g)), radius, new Metal(Vec3(0.5f, 0.5f, 0.5f), 0.f)));
+        } else {
+            float radius = r(g);
+            spheres.push_back(new Sphere(Vec3(p(g), radius, p(g)), radius, new Dielectric(Vec3(1, 1, 1), 1.5f)));
+        }
+    }
+    return spheres;
+}
+
 
 int main() {
     std::ofstream file("picture.ppm");
@@ -47,31 +70,34 @@ int main() {
     int numSample = 100;
     //int maxDepth = 50;
 
-    std::vector<Hittable *> objects;
-    objects.push_back(new Sphere(Vec3(2, 0, 0), 0.5, new Lambertian(Vec3(0.1, 0.9, 0.5))));
-    objects.push_back(new Sphere(Vec3(1, 0, 0), 0.5, new Dielectric(Vec3(0.6, 0.8, 1), 1.5)));
+//    std::vector<Hittable *> objects;
+//    objects.push_back(new Sphere(Vec3(2, 0, 0), 0.5, new Lambertian(Vec3(0.1, 0.9, 0.5))));
+//    objects.push_back(new Sphere(Vec3(1, 0, 0), 0.5, new Dielectric(Vec3(0.6, 0.8, 1), 1.5)));
 //  objects.push_back(new Sphere(Vec3(1, 0, 0), -0.4, new Dielectric(1.5)));
-    objects.push_back(new Sphere(Vec3(0, 0, 0), 0.5, new Lambertian(Vec3(0.5, 0.5, 0.5))));
-    objects.push_back(new Sphere(Vec3(-1, 0, 0), 0.5, new Metal(Vec3(0.9, 0.8, 0.2), 0.3)));
-    objects.push_back(new Sphere(Vec3(-2, 0, 0), 0.5, new Dielectric(Vec3(0.5, 1, 0.5), 1.5)));
-    objects.push_back(new Sphere(Vec3(-2, 0, 0), -0.4, new Dielectric(Vec3(1, 1, 1), 1.5)));
+//    objects.push_back(new Sphere(Vec3(0, 0, 0), 0.5, new Lambertian(Vec3(0.5, 0.5, 0.5))));
+//    objects.push_back(new Sphere(Vec3(-1, 0, 0), 0.5, new Metal(Vec3(0.9, 0.8, 0.2), 0.3)));
+//    objects.push_back(new Sphere(Vec3(-2, 0, 0), 0.5, new Dielectric(Vec3(0.5, 1, 0.5), 1.5)));
+//    objects.push_back(new Sphere(Vec3(-2, 0, 0), -0.4, new Dielectric(Vec3(1, 1, 1), 1.5)));
     //objects.push_back(new Sphere(Vec3(0, -100.5, 0), 100, new Lambertian(Vec3(1.0, 0.2, 0.1))));
 
-    HittableList world;
-    world.objectList = objects;
-
-    BVHNode bvh = BVHNode(objects);
+//    HittableList world;
+//    world.objectList = objects;
 
     Camera camera = Camera(
-            Vec3(0.5, 1, -2),
+            Vec3(0, 20, -60),
             Vec3(0, 0, 0),
             Vec3(0, 1, 0),
-            60,
+            40,
             float(width) / float(height),
-            0.1,
-            Vec3(0.5, 1, -2).norm()
+            2.0f,
+            60
     );
     std::default_random_engine randGenerator;
+    std::vector<Hittable *> spheres = randomSpheres(600, randGenerator);
+    BVHNode bvh(spheres);
+    HittableList world;
+    world.objectList = spheres;
+
     std::uniform_real_distribution<float> dist(0.0f, 1.0f);
 
     int progress = 0;
@@ -88,15 +114,18 @@ int main() {
                 float v = (float(j) + dist(randGenerator)) / float(height);
 
                 Ray ray = camera.getRay(u, v, randGenerator);
-                pixel += getColor(ray, world, 0, randGenerator, metadata);
+                pixel += getColor(ray, bvh, 0, randGenerator, metadata);
             }
             pixel /= float(numSample);
 
-            int ir = int(255.99 * powf(pixel.x, 1.f / 2));
-            int ig = int(255.99 * powf(pixel.y, 1.f / 2));
-            int ib = int(255.99 * powf(pixel.z, 1.f / 2));
-            file << ir << " " << ig << " " << ib << "\n";
+            int ir = int(255. * powf(pixel.x, 1.f / 2));
+            int ig = int(255. * powf(pixel.y, 1.f / 2));
+            int ib = int(255. * powf(pixel.z, 1.f / 2));
+            if (ir > 255 || ig > 255 || ib > 255) std::cout << pixel << std::endl;
+            if (ir < 0 || ig < 0 || ib < 0) std::cout << pixel << std::endl;
+            file << ir << " " << ig << " " << ib << " ";
         }
+        file << std::endl;
 
         ++progress;
         if (progress % (height / 100) == 0) {
@@ -110,7 +139,8 @@ int main() {
     std::cout << "\rRender duration: " << timeFormat(duration.count()) << std::endl;
     std::cout << "Num Object hit: " << std::setw(12) << std::setfill(' ') << metadata.numHitObject << std::endl;
     std::cout << "Num BVH hit:    " << std::setw(12) << std::setfill(' ') << metadata.numHitBHV << std::endl;
-    std::cout << "Total:          " << std::setw(12) << std::setfill(' ') << metadata.numHitBHV + metadata.numHitObject << std::endl;
+    std::cout << "Total:          " << std::setw(12) << std::setfill(' ') << metadata.numHitBHV + metadata.numHitObject
+              << std::endl;
 
     return EXIT_SUCCESS;
 }
@@ -123,10 +153,10 @@ Vec3 getColor(
         Metadata &metadata
 ) {
     RaycastHit hit;
-    if (world.hit(ray, 0.001f, std::numeric_limits<float>::max(), hit, metadata)) {
+    if (world.hit(ray, 0.1f, std::numeric_limits<float>::max(), hit, metadata)) {
         Ray newRay;
         Vec3 attenuation;
-        if (depth < 10 && hit.material->scatter(ray, hit, attenuation, newRay, generator)) {
+        if (depth < 20 && hit.material->scatter(ray, hit, attenuation, newRay, generator)) {
             return attenuation * getColor(newRay, world, depth + 1, generator, metadata);
         } else {
             return Vec3(0, 0, 0);
